8fa4de1b65c718ce72dd3faf1f52548e0c7d4572
sarfraz malik <sarfraz.malik@gmail.com>
2019-01-31 15:21:07 +0000

    Fixed script paramters

--- sample-api/scripts/AddProductToApiMgr.ps1
+++ sample-api/scripts/AddProductToApiMgr.ps1
@@ -1,17 +1,17 @@
 Param(
 [string] $apiManagementRg,
 [string] $apiManagementName,
-[string] $IRISProductID,
-[string] $IRISProductName,
-[bool] $ApprovalRequired=$True,
-[bool] $SubscriptionRequired=$True
+[string] $ProductID,
+[string] $ProductName,
+[string] $ProductDescription,
+[string] $LegalTerms
 )
 $DebugPreference="Continue"
 
 $ApiMgmtContext = New-AzureRmApiManagementContext -ResourceGroupName "$apiManagementRg" -ServiceName "$apiManagementName" 
 
-$product = Get-AzureRmApiManagementProduct -Context $ApiMgmtContext -ProductId $IRISProductID -ErrorAction Continue
+$product = Get-AzureRmApiManagementProduct -Context $ApiMgmtContext -ProductId $ProductID -ErrorAction SilentlyContinue
 
 if (!$product){
-New-AzureRmApiManagementProduct -Context $ApiMgmtContext -ProductId "$IRISProductID" -Title "$IRISProductName" -Description "Subscribers have unlimited access to the TFL Bus Operator Incident Reporting API's. Administrator approval is required." -LegalTerms "Transport For London all rights reserved" -ApprovalRequired $ApprovalRequired -State "Published" -SubscriptionRequired $SubscriptionRequired
+New-AzureRmApiManagementProduct -Context $ApiMgmtContext -ProductId "$ProductID" -Title "$ProductName" -Description $ProductDescription -LegalTerms $LegalTerms -ApprovalRequired $True -State "Published"
 }
