f2ee590f06e780e923e7a3dfcfb990b1602b8af0
sarfraz malik <sarfraz.malik@gmail.com>
2019-03-28 15:05:29 +0000

    added trace in eapim role creation script.

--- shared/roles/CreateEAPIMCustomRoles.ps1
+++ shared/roles/CreateEAPIMCustomRoles.ps1
@@ -3,11 +3,21 @@ Param(
 [string] $EAPIMReaderRoleDefinitionPath
 )
 
+Write-Verbose "Creating EAPIM Custom Roles"
+
+Write-Verbose $EAPIMReaderRoleName
+Write-Verbose $EAPIMReaderRoleDefinitionPath
+
+
 # Remove if the role exists. 
 Remove-AzureRmRoleDefinition  -Name $EAPIMReaderRoleName -Force
 
+Write-Verbose "Removed existing role"
+
 # Create the Role with the updated definition
 New-AzureRmRoleDefinition -InputFile $EAPIMReaderRoleDefinitionPath
+
+Write-Verbose "EAPIM Reader Role Added"
 
 # Check that the new role has been created
 Get-AzureRmRoleDefinition -Name $EAPIMReaderRoleName
