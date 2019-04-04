94ce399297be7d5bb6404bce44f832333cb367d0
sarfraz malik <sarfraz.malik@gmail.com>
2019-04-04 21:17:08 +0100

    corrected the import api functionality for an api with azure function backend.

--- sample-api/scripts/AddApiToAPIManagementFromSwaggerWithAzureFuncBackEnd.ps1
+++ sample-api/scripts/AddApiToAPIManagementFromSwaggerWithAzureFuncBackEnd.ps1
@@ -4,17 +4,13 @@ Param(
 [string] $apiId,
 [string] $swaggerFilePath,
 [string] $productID,
+[string] $apiPath = "apis",
 [string] $functionAppName,
 [string] $functionUrl ,
 [string] $functionKey, 
-[string] $protocol,
-[string] $apiPath = "apis",
+[string] $protocol 
 )
 
-#Login-AzureRmAccount
-#Select-AzureRmSubscription   -Subscription  "C106 - API Management Service - Test"
-
-$DebugPreference="Continue"
 
 if (!(Test-Path $swaggerFilePath)) {
     Write-Output "swagger file does not exist on the location"
@@ -25,10 +21,13 @@ Write-verbose "$ApiMgmtContext" -verbose
 $DebugPreference="Continue"
 
 
+$BackEnd = Get-AzureRmApiManagementBackend  -Context $ApiMgmtContext -BackendId $functionAppName
+$cred = $BackEnd.Credentials
+
 $existingApi = Get-AzureRmApiManagementApi -Context $ApiMgmtContext -ApiId $apiId -ErrorAction Continue
 
 if (!$existingApi) {
-   $credential = New-AzureRmApiManagementBackendCredential  -Header @{"x-functions-key" = @('$functionKey')}
+   $credential = New-AzureRmApiManagementBackendCredential -AuthorizationHeaderParameter opensesame  -Header @{"x-functions-key" = @('s113d01-rstone-fa-01-key')}
     New-AzureRmApiManagementBackend -Context $ApiMgmtContext -BackendId $functionAppName -Url $functionUrl -Protocol $protocol  -Credential $credential -Description $functionAppName
 
 }
@@ -36,7 +35,7 @@ else
 {
    Remove-AzureRmApiManagementApi -Context $ApiMgmtContext -ApiId $apiId -erroraction 'silentlycontinue'
    Remove-AzureRmApiManagementBackend -Context $ApiMgmtContext -BackendId $functionAppName
-   $credential = New-AzureRmApiManagementBackendCredential  -Header @{"x-functions-key" = @('$functionKey')}
+   $credential = New-AzureRmApiManagementBackendCredential -AuthorizationHeaderScheme basic -AuthorizationHeaderParameter opensesame  -Header @{"x-functions-key" = @($functionKey)}
    New-AzureRmApiManagementBackend  -Context $ApiMgmtContext -BackendId $functionAppName -Url $functionUrl -Protocol $protocol  -Credential $credential -Description $functionAppName
 
 }
