3a91dd29ee1e2c3df43b81ee6ea4029117111e37
sarfraz malik <sarfraz.malik@gmail.com>
2019-05-17 11:38:11 +0100

    fixed issue for adding produt into eapim.

--- sample-api/scripts/AddProductToApiMgr.ps1
+++ sample-api/scripts/AddProductToApiMgr.ps1
@@ -13,5 +13,11 @@ $ApiMgmtContext = New-AzureRmApiManagementContext -ResourceGroupName "$apiManage
 $product = Get-AzureRmApiManagementProduct -Context $ApiMgmtContext -ProductId $ProductID -ErrorAction SilentlyContinue
 
 if (!$product){
-New-AzureRmApiManagementProduct -Context $ApiMgmtContext -ProductId "$ProductID" -Title "$ProductName" -Description $ProductDescription -LegalTerms $LegalTerms -ApprovalRequired $True -State "Published"
+    New-AzureRmApiManagementProduct -Context $ApiMgmtContext -ProductId "$ProductID" -Title "$ProductName" -Description $ProductDescription -LegalTerms $LegalTerms -ApprovalRequired $True -State "Published"
+    write-host("Added Product into the EAPIM environment.")
+    # ADD Product into Developers Group
+    $DevGroup = Get-AzureRmApiManagementGroup  -Name "Developers" -Context $ApiMgmtContext
+    $APIMProduct = Get-AzureRmApiManagementProduct -Context $ApiMgmtContext -ProductId $ProductID
+    Add-AzureRmApiManagementProductToGroup -Context $ApiMgmtContext -GroupId  $DevGroup.GroupId -ProductId $APIMProduct.ProductId
+    Write-Output ("API Product - "+ $ProductID +" added into Developers Group.")
 }
